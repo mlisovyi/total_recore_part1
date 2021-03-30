6871a32785753e1df26fbdb763683cb51a1a0864
mlisovyi <mlisovyi@gmail.com>
2021-03-30 23:03:15 +0200

    various thresholds

--- train.py
+++ train.py
@@ -26,9 +26,61 @@ if abspath("/opt/ml/code") not in sys.path:
 
 
 class CustomRidgeCV(RidgeCV):
+    _v_min = [
+        -0.0,
+        -0.0,
+        -0.0,
+        30.0,
+        -0.0,
+        -0.0,
+        2.0,
+        3.0,
+        1.0,
+        76.0,
+        0.0,
+        3.0,
+        0.0,
+        -0.0,
+        100.0,
+        -0.0,
+        -1.0,
+        0.0,
+        6.0,
+        -50.0,
+        90.0,
+        -100.0,
+        0.0,
+        0.0,
+        -2.0,
+        1.0,
+        -0.0,
+        3100.0,
+        500.0,
+        1.0,
+        5.0,
+        0.0,
+        2.0,
+        -0.0,
+        0.0,
+        -0.0,
+        1000.0,
+        1.0,
+        1.0,
+        1100.0,
+        400.0,
+        0.0,
+        3.0,
+        -0.0,
+        1.0,
+        -0.0,
+        110.0,
+        0.0,
+        1.0,
+    ]
+
     def predict(self, X):
         y_preds = super().predict(X)
-        y_preds = np.clip(y_preds, 0, None)
+        y_preds = np.clip(y_preds, self._v_min, None)
         return y_preds
 
 
