bdc84602d54a0869671cea8671e01444df248525
mlisovyi <mlisovyi@gmail.com>
2021-03-30 21:31:46 +0200

    cleanup

--- eda.py
+++ eda.py
@@ -10,10 +10,8 @@ from predict import preprocess
 data_dir = getenv("SM_CHANNEL_TRAINING", "/opt/ml/input/data/training")
 data_file = join(data_dir, "public.csv.gz")
 df_pp = preprocess(data_file)
-df = pd.read_csv(
-    data_file,
-    index_col=0,
-)
+df = pd.read_csv(data_file, index_col=0,)
+y_oof = pd.read_csv(join(data_dir, "oof.csv"), index_col=0,)
 # %%
 df_min_depth = df.groupby("SiteID")["DepthFrom"].min()
 df_max_depth = df.groupby("SiteID")["DepthTo"].max()
@@ -37,7 +35,7 @@ plt.title("Top CuPPM")
 plt.show()
 
 # %%
-n = 5
+n = 10
 df_ = df_pp.sort_values(by="CuPPM").head(n)
 for i in range(n):
     df_.iloc[i, 3:-49].plot(label=str(i))
