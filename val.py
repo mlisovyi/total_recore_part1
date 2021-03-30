c2b06451740ea173a5fcc277bd2a8810a0c276fa
mlisovyi <mlisovyi@gmail.com>
2021-03-30 21:33:34 +0200

    use custom ridge with 0 clipping

--- val.py
+++ val.py
@@ -24,8 +24,9 @@ from sklearn.linear_model import (
 from sklearn.metrics import make_scorer, r2_score
 from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 
-from preprocess import preprocess
+from preprocess import preprocess, target_columns
 from score import scoring_fn
+from train import CustomRidgeCV, save_model, model_fn, input_fn
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -36,59 +37,6 @@ logging.basicConfig(level=logging.INFO)
 if abspath("/opt/ml/code") not in sys.path:
     sys.path.append(abspath("/opt/ml/code"))
 
-    # the list of 49 elements which are target variables for this challenge
-    target_columns = [
-        "AgPPM",
-        "AsPPM",
-        "AuPPM",
-        "BaPPM",
-        "BiPPM",
-        "CdPPM",
-        "CoPPM",
-        "CuPPM",
-        "FePCT",
-        "MnPPM",
-        "MoPPM",
-        "NiPPM",
-        "PPCT",
-        "PbPPM",
-        "SPPM",
-        "SbPPM",
-        "SePPM",
-        "SnPPM",
-        "SrPPM",
-        "TePPB",
-        "ThPPB",
-        "UPPB",
-        "VPCT",
-        "WPPM",
-        "ZnPPM",
-        "ZrPPM",
-        "BePPM",
-        "AlPPM",
-        "CaPPM",
-        "CePPM",
-        "CrPPM",
-        "CsPPM",
-        "GaPPM",
-        "GePPM",
-        "HfPPM",
-        "InPPM",
-        "KPPM",
-        "LaPPM",
-        "LiPPM",
-        "MgPPM",
-        "NaPPM",
-        "NbPPM",
-        "RbPPM",
-        "RePPM",
-        "ScPPM",
-        "TaPPM",
-        "TiPPM",
-        "TlPPM",
-        "YPPM",
-    ]
-
 
 def train_evaluate_model(X_trn, X_tst, y_trn, y_tst, verbose=True):
     # if verbose:
@@ -97,7 +45,7 @@ def train_evaluate_model(X_trn, X_tst, y_trn, y_tst, verbose=True):
     # the example model
     # model = RidgeCV(alphas=(1e-2, 1e-1, 1, 1e1, 1e2), scoring=make_scorer(r2_score))
     # model = MultiTaskLassoCV()
-    model = RidgeCV(
+    model = CustomRidgeCV(
         alphas=np.logspace(-5, 5, 50),
         scoring=make_scorer(r2_score),
         alpha_per_target=True,
@@ -114,12 +62,12 @@ def train_evaluate_model(X_trn, X_tst, y_trn, y_tst, verbose=True):
     score_tst = scoring_fn(y_tst, preds_tst)
 
     if verbose:
-        scores_series = pd.Series(
-            scoring_fn(y_tst, preds_tst, True), index=target_columns
-        )
-        print(scores_series.sort_values().tail())
+        # scores_series = pd.Series(
+        #     scoring_fn(y_tst, preds_tst, True), index=target_columns
+        # )
+        # print(scores_series.sort_values().tail())
         print(f"SCORE: TRN = {score_trn:.3f}, TST = {score_tst:.3f}")
-    return model, score_trn, score_tst
+    return model, score_trn, score_tst, preds_tst
 
 
 def train(args):
@@ -147,6 +95,7 @@ def train(args):
         _ = train_evaluate_model(X_trn, X_tst, y_trn, y_tst)
     else:
         scores = []
+        y_oof = np.zeros_like(y_train.values)
         cv = KFold(n_splits=4, random_state=42, shuffle=True)
         for i, (idx_trn, idx_tst) in enumerate(cv.split(X_train, y_train)):
             logger.info(f"------ {i} -------------")
@@ -154,51 +103,18 @@ def train(args):
             X_tst, y_tst = X_train.iloc[idx_tst, :], y_train.iloc[idx_tst]
             # allow usage a subsampling to check dependence of performace on data sample
             n = int(len(y_trn) * 1)
-            _, _, score_tst = train_evaluate_model(
-                X_trn.head(n), X_tst, y_trn.head(n), y_tst, verbose=False
+            _, _, score_tst, preds_tst = train_evaluate_model(
+                X_trn.head(n), X_tst, y_trn.head(n), y_tst, verbose=True
             )
             scores.append(score_tst)
+            y_oof[idx_tst, :] = preds_tst.values
 
         print(f"Performance in CV: {np.mean(scores):.3f} +- {np.std(scores):.3f}")
+        y_oof = pd.DataFrame(y_oof, index=y_train.index, columns=y_train.columns)
+        y_oof.to_csv(join(args.data_dir, "oof.csv"))
 
     # save the model to disk)  #
     # save_model(model, args.model_dir)
-
-
-def save_model(model, model_dir):
-    """Save model to a binary file.
-
-    This function must write the model to disk in a format that can
-    be loaded from the model_fn.
-
-    WARNING - modifying this function may cause the submission process to fail.
-    """
-    logger.info(f"saving model to {model_dir}")
-    with open(join(model_dir, "model.pkl"), "wb") as model_file:
-        pickle.dump(model, model_file)
-
-
-def model_fn(model_dir):
-    """Load model from binary file.
-
-    This function loads the model from disk. It is called by SageMaker.
-
-    WARNING - modifying this function may case the submission process to fail.
-    """
-    logger.info("loading model")
-    with open(join(model_dir, "model.pkl"), "rb") as file:
-        return pickle.load(file)
-
-
-def input_fn(input_data, content_type):
-    """Take request data and de-serialize the data into an object for prediction.
-
-    In the Unearthed submission pipeline the data is passed as "text/csv". This
-    function reads the CSV into a Pandas dataframe ready to be passed to the model.
-
-    WARNING - modifying this function may cause the submission process to fail.
-    """
-    return pd.read_csv(StringIO(input_data), index_col=0)
 
 
 if __name__ == "__main__":
@@ -221,8 +137,6 @@ if __name__ == "__main__":
         default=getenv("SM_CHANNEL_TRAINING", "/opt/ml/input/data/training"),
     )
     parser.add_argument(
-        "--cv",
-        default=False,
-        action="store_true",
+        "--cv", default=False, action="store_true",
     )
     train(parser.parse_args())
